93cb88d8912210076ea65137f2f008b8e68a7a14
Mohammad <mamro190@gmail.com>
2022-04-19 02:06:12 +0300

    header img

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,31 +1,32 @@
 import React from "react";
 import "./Home.scss";
+import hero from "../../assets/hero.png";
 
 function Home() {
   return (
-    <div class="container-xxl py-5 bg-dark hero-header mb-5">
-      <div class="container my-5 py-5">
-        <div class="row align-items-center g-5">
-          <div class="col-lg-6 text-center text-lg-start">
-            <h1 class="display-3 text-white animated slideInLeft">
+    <div className="container-xxl py-5 bg-dark hero-header mb-5">
+      <div className="container my-5 py-5">
+        <div className="row align-items-center g-5">
+          <div className="col-lg-6 text-center text-lg-start">
+            <h1 className="display-3 text-white animated slideInLeft">
               Enjoy Our
               <br />
               Delicious Meal
             </h1>
-            <p class="text-white animated slideInLeft mb-4 pb-2">
+            <p className="text-white animated slideInLeft mb-4 pb-2">
               Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu
               diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet
               lorem sit clita duo justo magna dolore erat amet
             </p>
             <a
               href=""
-              class="btn btn-primary primaryBtn py-sm-3 px-sm-5 me-3 animated slideInLeft"
+              className="btn btn-primary primaryBtn py-sm-3 px-sm-5 me-3 animated slideInLeft"
             >
               Book A Table
             </a>
           </div>
-          <div class="col-lg-6 text-center text-lg-end overflow-hidden">
-            <img class="img-fluid" src="img/hero.png" alt="" />
+          <div className="col-lg-6 text-center text-lg-end overflow-hidden">
+            <img className="img-fluid" src={hero} alt="" />
           </div>
         </div>
       </div>
