38312de7171d555d55fadae4d3b4c6a59146be8e
Mohammad <mamro190@gmail.com>
2022-04-20 22:44:07 +0300

    contact page

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,12 @@
+import React from "react";
+import BreadCrumb from "../AssetComponents/BreadCrumb/BreadCrumb";
+
+function Contact() {
+  return (
+    <div className="contact">
+      <BreadCrumb title={"Contact Us"} pageName={"Contact"} />
+    </div>
+  );
+}
+
+export default Contact;
